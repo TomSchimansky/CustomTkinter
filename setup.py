0c8e06b1508db3d7f64c8f4e95e84890a65de1da
Tom Schimansky <tomschimansk@gmail.com>
2021-05-08 12:03:43 +0200

    minor changes for pypi

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(name="customtkinter",
+      version="0.5",
+      author="Tom Schimansky",
+      license="Creative Commons Zero v1.0 Universal",
+      url="https://github.com/TomSchimansky/CustomTkinter",
+      description="Create modern looking gui with tkinter and python",
+      long_description_content_type="text/markdown",
+      long_description=read('Readme_pypi.md'),
+      packages=["customtkinter"],
+      classifiers=["Operating System :: OS Independent",
+                   "Programming Language :: Python :: 3",
+                   "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"],
+      install_requires=["darkdetect"])
