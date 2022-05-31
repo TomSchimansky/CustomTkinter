0aa9dfc70fe009765cc2d7b3df504657213e98b9
Tom Schimansky <tomschimansk@gmail.com>
2022-05-31 23:16:13 +0200

    removed combobox from simple_example.py

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -42,10 +42,6 @@ optionmenu_1 = customtkinter.CTkOptionMenu(frame_1, values=["Option 1", "Option 
 optionmenu_1.pack(pady=10, padx=10)
 optionmenu_1.set("CTkOptionMenu")
 
-combobox_1 = customtkinter.CTkComboBox(frame_1, values=["Option 1", "Option 2", "Option 42"])
-combobox_1.pack(pady=10, padx=10)
-combobox_1.set("CTkComboBox")
-
 checkbox_1 = customtkinter.CTkCheckBox(master=frame_1)
 checkbox_1.pack(pady=y_padding, padx=10)
 
