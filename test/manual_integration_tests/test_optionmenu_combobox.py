ecf6b8d9cf786d9191e987e21f24e5fee60c1912
Tom Schimansky <tomschimansk@gmail.com>
2022-06-02 01:25:13 +0200

    added combobox values to themes

--- test/manual_integration_tests/test_optionmenu_combobox.py
+++ test/manual_integration_tests/test_optionmenu_combobox.py
@@ -1,9 +1,8 @@
 import tkinter
-import tkinter.ttk as ttk
 import customtkinter
 
 app = customtkinter.CTk()
-app.title('test_optionmenu.py')
+app.title('Test OptionMenu ComboBox.py')
 app.geometry('400x300')
 
 
@@ -17,16 +16,16 @@ countries = ['Bahamas', 'Canada', 'Cuba', 'United States']
 variable = tkinter.StringVar()
 variable.set("test")
 
-optionmenu_tk = tkinter.OptionMenu(app, variable, *countries, command=select_callback)
-optionmenu_tk.pack(pady=10, padx=10)
+# optionmenu_tk = tkinter.OptionMenu(app, variable, *countries, command=select_callback)
+# optionmenu_tk.pack(pady=10, padx=10)
 
 optionmenu_1 = customtkinter.CTkOptionMenu(app, variable=variable, values=countries, command=select_callback)
-optionmenu_1.pack(pady=10, padx=10)
+optionmenu_1.pack(pady=20, padx=10)
 
-combobox_tk = ttk.Combobox(app, values=countries)
-combobox_tk.pack(pady=10, padx=10)
+# combobox_tk = ttk.Combobox(app, values=countries)
+# combobox_tk.pack(pady=10, padx=10)
 
 combobox_1 = customtkinter.CTkComboBox(app, variable=variable, values=countries, command=select_callback)
-combobox_1.pack(pady=10, padx=10)
+combobox_1.pack(pady=20, padx=10)
 
 app.mainloop()
