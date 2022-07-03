4f744c6566b999b55fd99785dff1755b7b667447
FTE <2336812+felipetesc@users.noreply.github.com>
2022-07-03 19:19:49 -0300

    ctk font for menu new version

--- customtkinter/widgets/ctk_menu.py
+++ customtkinter/widgets/ctk_menu.py
@@ -1,10 +1,4 @@
 import tkinter
-import sys
-import copy
-import re
-from typing import Union
-from typing import Callable
-
 from ..theme_manager import ThemeManager
 from ..appearance_mode_tracker import AppearanceModeTracker
 from ..scaling_tracker import ScalingTracker
@@ -23,7 +17,7 @@ class CTkMenu(tkinter.Menu, CTkBaseClass):
     def __init__(self, master: tkinter.Tk, text_font = "default_theme", cnf={}, **kw):
         
         super().__init__(cnf, kw)
-        
+
         if isinstance(master, tkinter.Tk) or isinstance(master, CTkFrame) or isinstance(master, CTkToplevel):
             self.option_add('*tearOff', False)
         
@@ -32,9 +26,9 @@ class CTkMenu(tkinter.Menu, CTkBaseClass):
         ctk_fg = self.get_color_from_name(self.mode, "text")
         ctk_hover_bg = self.get_color_from_name(self.mode, "button")
         self.text_font = (ThemeManager.theme["text"]["font"], ThemeManager.theme["text"]["size"]) if text_font == "default_theme" else text_font
-        #self.font = CTkBaseClass.apply_font_scaling(self.text_font)
+        self.font = CTkBaseClass.apply_font_scaling(self.text_font)
         self.configure(background=ctk_bg)
         self.configure(foreground=ctk_fg)
         self.configure(borderwidth=0)
         self.configure(activebackground=ctk_hover_bg)
-        self.config(font=self.text_font)
+        self.config(font=self.font)
