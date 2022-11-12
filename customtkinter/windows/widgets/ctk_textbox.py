3f44877984a0ed4d2cee78ae7a606c8c93b34609
TomSchimansky <tomschimansky@gmail.com>
2022-11-12 23:17:37 +0100

    fixed small bugs

--- customtkinter/windows/widgets/ctk_textbox.py
+++ customtkinter/windows/widgets/ctk_textbox.py
@@ -184,7 +184,7 @@ class CTkTextbox(CTkBaseClass):
         self._textbox.configure(font=self._apply_font_scaling(self._font))
         self._canvas.configure(width=self._apply_widget_scaling(self._desired_width),
                                height=self._apply_widget_scaling(self._desired_height))
-        self._create_grid_for_text_and_scrollbars(re_grid_textbox=False, re_grid_x_scrollbar=True, re_grid_y_scrollbar=True)
+        self._create_grid_for_text_and_scrollbars(re_grid_textbox=True, re_grid_x_scrollbar=True, re_grid_y_scrollbar=True)
         self._draw(no_color_updates=True)
 
     def _set_dimensions(self, width=None, height=None):
