e116faf9108737ebe127236913286b0beb035871
Tom Schimansky <tomschimansky@gmail.com>
2023-06-20 13:38:47 +0200

    add font option to input dialog #838

--- customtkinter/windows/ctk_input_dialog.py
+++ customtkinter/windows/ctk_input_dialog.py
@@ -5,6 +5,7 @@ from .widgets import CTkEntry
 from .widgets import CTkButton
 from .widgets.theme import ThemeManager
 from .ctk_toplevel import CTkToplevel
+from .widgets.font import CTkFont
 
 
 class CTkInputDialog(CTkToplevel):
@@ -24,6 +25,7 @@ class CTkInputDialog(CTkToplevel):
                  entry_text_color: Optional[Union[str, Tuple[str, str]]] = None,
 
                  title: str = "CTkDialog",
+                 font: Optional[Union[tuple, CTkFont]] = None,
                  text: str = "CTkDialog"):
 
         super().__init__(fg_color=fg_color)
@@ -39,9 +41,11 @@ class CTkInputDialog(CTkToplevel):
 
         self._user_input: Union[str, None] = None
         self._running: bool = False
+        self._title = title
         self._text = text
+        self._font = font
 
-        self.title(title)
+        self.title(self._title)
         self.lift()  # lift window on top
         self.attributes("-topmost", True)  # stay on top
         self.protocol("WM_DELETE_WINDOW", self._on_closing)
@@ -50,7 +54,6 @@ class CTkInputDialog(CTkToplevel):
         self.grab_set()  # make other windows not clickable
 
     def _create_widgets(self):
-
         self.grid_columnconfigure((0, 1), weight=1)
         self.rowconfigure(0, weight=1)
 
@@ -59,14 +62,16 @@ class CTkInputDialog(CTkToplevel):
                                wraplength=300,
                                fg_color="transparent",
                                text_color=self._text_color,
-                               text=self._text,)
+                               text=self._text,
+                               font=self._font)
         self._label.grid(row=0, column=0, columnspan=2, padx=20, pady=20, sticky="ew")
 
         self._entry = CTkEntry(master=self,
                                width=230,
                                fg_color=self._entry_fg_color,
                                border_color=self._entry_border_color,
-                               text_color=self._entry_text_color)
+                               text_color=self._entry_text_color,
+                               font=self._font)
         self._entry.grid(row=1, column=0, columnspan=2, padx=20, pady=(0, 20), sticky="ew")
 
         self._ok_button = CTkButton(master=self,
@@ -76,6 +81,7 @@ class CTkInputDialog(CTkToplevel):
                                     hover_color=self._button_hover_color,
                                     text_color=self._button_text_color,
                                     text='Ok',
+                                    font=self._font,
                                     command=self._ok_event)
         self._ok_button.grid(row=2, column=0, columnspan=1, padx=(20, 10), pady=(0, 20), sticky="ew")
 
@@ -86,6 +92,7 @@ class CTkInputDialog(CTkToplevel):
                                         hover_color=self._button_hover_color,
                                         text_color=self._button_text_color,
                                         text='Cancel',
+                                        font=self._font,
                                         command=self._cancel_event)
         self._cancel_button.grid(row=2, column=1, columnspan=1, padx=(10, 20), pady=(0, 20), sticky="ew")
 
