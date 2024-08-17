a5aeb05c9296e1bc986fc83bd5a2e83f7c851e7f
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-08-17 16:38:50 +0800

    add grid_configure to specify the margin(padx/pady) of the _parent_canvas while scrollbar isn't displayed

--- customtkinter/windows/widgets/ctk_scrollable_frame.py
+++ customtkinter/windows/widgets/ctk_scrollable_frame.py
@@ -248,13 +248,17 @@ class CTkScrollableFrame(tkinter.Frame, CTkAppearanceModeBaseClass, CTkScalingBa
                 self._parent_frame.cget("corner_radius") + self._parent_frame.cget("border_width"))
         if self._orientation == "horizontal":
             if self._parent_canvas.xview() != (0.0, 1.0):
+                self._parent_canvas.grid_configure(pady=(border_spacing, 0))
                 self._scrollbar.grid(row=2, column=0, sticky="nsew", padx=border_spacing)
             else:
+                self._parent_canvas.grid_configure(pady=border_spacing)
                 self._scrollbar.grid_remove()
         elif self._orientation == "vertical":
             if self._parent_canvas.yview() != (0.0, 1.0):
+                self._parent_canvas.grid_configure(padx=(border_spacing, 0))
                 self._scrollbar.grid(row=1, column=1, sticky="nsew", padx=border_spacing)
             else:
+                self._parent_canvas.grid_configure(padx=border_spacing)
                 self._scrollbar.grid_remove()
 
     def _set_scroll_increments(self):
